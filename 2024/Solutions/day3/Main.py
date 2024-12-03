a88edd4d715a8920041945a8a6c77e8c40f42d84
laurynukas98 <63808873+laurynukas98@users.noreply.github.com>
2024-12-03 20:10:19 +0200

    improve readability

--- 2024/Solutions/day3/Main.py
+++ 2024/Solutions/day3/Main.py
@@ -4,20 +4,27 @@ import re
 sys.path.append("../../../Helpers")
 from CommonShitz import readFile
 
-def regex_rizz_found_part1(line) -> list[str]:
+def regex_rizz_found_part1(line: str) -> list[str]:
     """part 3 regex."""
     return re.findall(r"mul\(\d{1,3}\,\d{1,3}\)", line)
 
-def regex_rizz_found_part2(line) -> list[str]:
+def regex_rizz_found_part2(line: str) -> list[str]:
     """part 2 regex."""
     test = re.sub(r"(don't\(\)).*?(do\(\))","", line)
     test = re.sub(r"(don't\(\)).*","", test)
     return re.findall(r"mul\(\d{1,3}\,\d{1,3}\)", test)
 
-def multiplicationz(line) -> int:
+def multiplicationz(line: str) -> int:
     """do multiplication."""
     test = re.match(r"mul\((.*),(.*)\)", line)
     return int(test.group(1)) * int(test.group(2))
+
+def calc_sum(found: list[str]) -> int:
+    """calc sum of all multiplications."""
+    rez = 0
+    for x in found:
+        rez += multiplicationz(x)
+    return rez
 
 def start():
     """starts masterpiece."""
@@ -25,22 +32,14 @@ def start():
     print('Part1 rez: ' + str(part1(data)))
     print('Part2 rez: ' + str(part2(data)))
 
-def part1(data) -> str:
+def part1(data: list[str]) -> str:
     """starts masterpiece part 1"""
-    found = []
-    for line in data:
-        found += regex_rizz_found_part1(line)
-    rez = 0
-    for x in found:
-        rez += multiplicationz(x)
-    return rez
+    found = regex_rizz_found_part1("".join(data))
+    return calc_sum(found)
 
-def part2(data) -> str:
+def part2(data: list[str]) -> str:
     """starts masterpiece part 2"""
     found = regex_rizz_found_part2("".join(data))
-    rez = 0
-    for x in found:
-        rez += multiplicationz(x)
-    return rez
+    return calc_sum(found)
 
 start()
