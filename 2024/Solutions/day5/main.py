91e8369561fc153c5e7cfcbacf1676d483fe3719
laurynukas98 <63808873+laurynukas98@users.noreply.github.com>
2024-12-05 18:43:45 +0200

    simplified day 5 2024

--- 2024/Solutions/day5/main.py
+++ 2024/Solutions/day5/main.py
@@ -4,7 +4,7 @@ def ordering_check_part1(rules, page):
     for i, char in enumerate(page):
         for (x, y) in rules:
             if char == x:
-                for ib, charr in enumerate(page[:i]):
+                for _, charr in enumerate(page[:i]):
                     if charr == y:
                         return False
     return True
@@ -17,12 +17,10 @@ def build_recursion(rules, start, array_of_available_values):
         else:
             return ''
     for x in range(len(start) + 1):
-        testt = start.copy()
-        testt.insert(x, array_of_available_values[0])
-        if ordering_check_part1(rules, testt):
-            if (len(array_of_available_values)) == 0:
-                return testt
-            value = build_recursion(rules, testt, array_of_available_values[1:])
+        copy_start = start.copy()
+        copy_start.insert(x, array_of_available_values[0])
+        if ordering_check_part1(rules, copy_start):
+            value = build_recursion(rules, copy_start, array_of_available_values[1:])
             if value != '':
                 return value
     return ''
@@ -32,8 +30,7 @@ def ordering_check_part2(rules, page) -> str:
         return build_recursion(rules, [page[0]], page[1:])
     return ''
 
-def part1(data) -> str:
-    """starts masterpiece part 1"""
+def prepare_data(data):
     switch_flag = False
     rules = []
     pages = []
@@ -43,39 +40,24 @@ def part1(data) -> str:
             continue
         if switch_flag:
             pages.append(line.split(','))
-        if not switch_flag:
+        else:
             matched = line.split('|')
             rules.append((matched[0],matched[1]))
+    return (rules, pages)
 
+def calc_rez(functionz, data):
+    (rules, pages) = prepare_data(data)
     rez = 0
-    i = 0
     for page in pages:
-        if ordering_check_part1(rules, page):
-            rez += int(page[len(page)//2])
-        else:
-            i += 1
-    print('Part 1 not working: ' + str(i))
-
+        value = functionz(rules, page)
+        if isinstance(value, list) or value is True:
+            rez += int(value[len(value)//2] if isinstance(value, list) else page[len(page)//2])
     return rez
+
+def part1(data) -> str:
+    """starts masterpiece part 1"""
+    return calc_rez(ordering_check_part1, data)
 
 def part2(data) -> str:
     """starts masterpiece part 2"""
-    switch_flag = False
-    rules = []
-    pages = []
-    for line in data:
-        if line == '':
-            switch_flag = True
-            continue
-        if switch_flag:
-            pages.append(line.split(','))
-        if not switch_flag:
-            matched = line.split('|')
-            rules.append((matched[0],matched[1]))
-
-    rez = 0
-    for page in pages:
-        value = ordering_check_part2(rules, page)
-        if value != '':
-            rez += int(value[len(value)//2])
-    return rez
+    return calc_rez(ordering_check_part2, data)
