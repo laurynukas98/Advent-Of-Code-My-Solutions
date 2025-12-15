821a539866c8c48e8163659a5fcc06362f87cc39
Laurynukas98 <63808873+laurynukas98@users.noreply.github.com>
2025-12-15 16:18:36 +0200

    solve day 3 2025

--- 2025/Solutions/day3/main.py
+++ 2025/Solutions/day3/main.py
@@ -10,51 +10,26 @@ def part1(data) -> str:
                 calc = int(line[x] + line[y])
                 if calc > maxJolt:
                     maxJolt = calc
-        print(maxJolt)
         rez += maxJolt
     return rez
 
-def idr_how_this_is_called(line, size, val, max):
+def idr_how_this_is_called(line, size, val, max): # Optimise because too sloowww
     test = max
     if (size == 0):
         if (test < int(val)):
             return int(val)
+        return test
+    if (val != '' and test > int(val) * (10**size)):
         return test
     if len(line) > 0 and len(line) >= size:
         for x in range(0, len(line)):
             test = idr_how_this_is_called(line[x + 1:], size - 1, val + line[x], test)
     return test
 
-def part2_stupid_solution(data) -> str:
-    """starts masterpiece part 2"""
-    rez = 0
-    for line in data:
-        valuez = idr_how_this_is_called(line, 12, "", 0)
-        print("-------")
-        print(valuez)
-        print("-------")
-        rez += valuez
-    return rez
-
 def part2(data) -> str:
     """starts masterpiece part 2"""
     rez = 0
     for line in data:
-        rezValue = ""
-        size = 12
-        set_chars = {key:(line.count(key)) for key in sorted(set(line), reverse=True)}
-        toFind = {}
-        for key,value in set_chars.items():
-            if (size - value > 0):
-                toFind[key] = value
-                size -= value
-            else:
-                toFind[key] = size
-                break
-        for c in line: # Nope, this doesn't work
-            if toFind[c] > 0:
-                rezValue += c
-                toFind[c] -= 1
-        print(rezValue)
-        rez += int(rezValue)
-    return 'Not Implemented'
+        valuez = idr_how_this_is_called(line, 12, "", 0)
+        rez += valuez
+    return rez
