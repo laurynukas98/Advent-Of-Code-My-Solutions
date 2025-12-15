b4cbbd678e017a8eb2d3b3adc5ed384bc38f8664
Laurynukas98 <63808873+laurynukas98@users.noreply.github.com>
2025-12-15 16:57:59 +0200

    cache to the rescue day 7 2025

--- 2025/Solutions/day7/main.py
+++ 2025/Solutions/day7/main.py
@@ -19,37 +19,22 @@ def part1(data) -> str:
         drops = list(set(new_drops))
     return split
 
+cache = {}
+
 def down(data, level, point):
     rez = 0
     if (point < 0) and (point > len(data[0]) - 1):
-        return 0
+        return rez
     for i in range(level, len(data)):
         if data[i][point] == '^':
-            rez += down(data, i + 1, point - 1)
-            rez += down(data, i + 1, point + 1)
-            rez += 1
+            if f"{point},{i}" not in cache:
+                cache[f"{point},{i}"] = down(data, i + 1, point - 1)
+                cache[f"{point},{i}"] += down(data, i + 1, point + 1)
+            rez += cache[f"{point},{i}"]
             return rez
-    return rez
+    return 1
 
 def part2(data) -> str:
     """starts masterpiece part 2"""
     index = data[0].find("S")
-    drops = [index]
-    split = 0
-    print(down(data, 0, index) + 1)
-    #for l in data[1:]:
-    #    gee = 0
-    #    new_drops = []
-    #    for drop in drops:
-    #        if l[drop] == "^":
-    #            gee += 1
-    #            if (drop - 1 > -1):
-    #                new_drops.append(drop - 1)
-    #            if (drop + 1 < len(data[0])):
-    #                new_drops.append(drop + 1)
-    #        elif l[drop] == ".":
-    #            new_drops.append(drop)
-    #    drops = list(set(new_drops))
-    #    split += gee * 2
-    print(split)
-    return 'Not Implemented'
+    return down(data, 0, index)
